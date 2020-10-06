a9515f3fbb8c9752c0893c687a74e3473265e55a
nbendzsa <70988618+nbendzsa@users.noreply.github.com>
2020-10-06 13:11:45 -0230

    Started Compression Sheet

---
+++ compression.table.js
@@ -0,0 +1,346 @@
+let compression_info = [{
+    'Filename': 'Filename',
+    'Compression algorithm': 'Compression algorithm',
+    'Bit-rate': 'Bit-rate',
+    'Original size': 'Original Size',
+    'Compressed size': 'Compressed Size',
+    'Compression Ratio': 'Compression Ratio',
+    'Link to original audio': 'Link to original audio',
+    'Link to compressed audio': 'Link to compressed audio',
+    'Link to residual audio': "Link to residual audio"
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }, {
+    'Filename': 'Please Please Me',
+    'Compression algorithm': 1963,
+    'Bit-rate': "Parlophone",
+    'Original size': '',
+    'Compressed size': '',
+    'Compression Ratio': '',
+    'Link to original audio': '',
+    'Link to compressed audio': '',
+    'Link to residual audio': ""
+  }]
+  function compression_table() {
+    let table = document.createElement('table');
+    table.id = "compression_table"
+    for (row = 0; row < compression_info.length; row++) {
+      tr = document.createElement('tr');
+      let compression_data = Object.keys(compression_info[row]);
+      for (col = 0; col < Object.keys(compression_info[row]).length; col++) {
+        td = document.createElement('td');
+        tr.appendChild(td);
+        td.innerHTML = compression_info[row][compression_data[col]];
+      }
+      table.appendChild(tr);
+    }
+    document.getElementById("discography_table").appendChild(table);
+  }
